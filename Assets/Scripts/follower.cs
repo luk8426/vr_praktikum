d525936a9d7d7746f6a2d9fe4ecb7a78ffa07e62
luk8426 <luk8426@thi.de>
2023-03-24 17:48:25 +0100

    Video

--- Assets/Scripts/follower.cs
+++ Assets/Scripts/follower.cs
@@ -12,10 +12,10 @@ public class follower : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         if (System.Math.Abs(Player.transform.position.x - transform.position.x) <= 1){
-            Vector3 direction = new Vector3(Player.transform.position.x - transform.position.x, 0, Player.transform.position.z - transform.position.z);
+            Vector3 direction = new Vector3(Player.transform.position.x - transform.position.x, Player.transform.position.y - transform.position.y, Player.transform.position.z - transform.position.z);
             direction.Normalize();
             transform.position = transform.position + (direction / 300);
         }
