7f056b91cdcc48eff45781ee2a87142e913e655c
luk8426 <luk8426@thi.de>
2023-04-20 03:28:34 +0200

    Airport + Village

--- Assets/Scripts/Cargo.cs
+++ Assets/Scripts/Cargo.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class Cargo : MonoBehaviour
 {
-    private MeshRenderer render;
+    //public MeshRenderer render;
+    public Rigidbody c_rigidbody;
     private Vector3 position;
     // Start is called before the first frame update
     void Start()
     {
         position = this.transform.position;
-        render = gameObject.GetComponentInChildren<MeshRenderer>();
- 
-        render.enabled = false;
+        //render.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire2")){
-            this.GetComponent<Rigidbody>().useGravity = true;
+        
+        if (Input.GetKey(KeyCode.K)){
+        //if (Input.GetButtonDown("Fire2")){
+            //render.enabled = true;
+            c_rigidbody.useGravity = true;
         }
 
 
